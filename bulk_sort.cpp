1b4618ff747569ac890cb17e5c18493527f89c72
schrky <Kyle.Schroetlin@wallawalla.edu>
2026-05-14 23:30:00 -0700

    Bulksort helper func

--- bulk_sort.cpp
+++ bulk_sort.cpp
@@ -7,12 +7,68 @@ void bulkSort(ikeaData& wh, std::string sortBy, size_t stopNum) {
     std::iota(indices.begin(), indices.end(), 0);
 
     std::sort(indices.begin(), indices.end(), [&](size_t a, size_t b) {
-        if (sortBy == "id") return (wh.id.at(a) < wh.id.at(b));
-        if (sortBy == "name") return (wh.name.at(a) < wh.name.at(b));
-        if (sortBy == "type") return (wh.type.at(a) < wh.type.at(b));
-        if (sortBy == "price") return (wh.price.at(a) < wh.price.at(b));
-        return (wh.id.at(a) < wh.id.at(b));
-    });
+        if (sortBy == "id") {
+            std::sort(indices.begin(), indices.end(),
+                [&](size_t a, size_t b) {
+                    return wh.id[a] < wh.id[b];
+                });
+            }
+        else if (sortBy == "name") {
+            std::sort(indices.begin(), indices.end(),
+                [&](size_t a, size_t b) {
+                    return wh.name[a] < wh.name[b];
+                });
+            }
+        else if (sortBy == "type") {
+            std::sort(indices.begin(), indices.end(),
+                [&](size_t a, size_t b) {
+                    return wh.type[a] < wh.type[b];
+                });
+            }
+        else if (sortBy == "price") {
+            std::sort(indices.begin(), indices.end(),
+                [&](size_t a, size_t b) {
+                    return wh.price[a] < wh.price[b];
+                });
+            }
 
-    // Update wh based on indices
+    });
+}
+
+void applyPermutation(ikeaData& wh, std::vector<size_t>& indices) {
+    size_t n = indices.size();
+
+    for (size_t i = 0; i < n; ++i) {
+
+        if (indices[i] == i)
+            continue;
+        
+        size_t current = i;
+
+        int tempId = wh.id[i];
+        std::string tempName = std::move(wh.name[i]);
+        std::string tempType = std::move(wh.type[i]);
+        double tempPrice = wh.price[i];
+
+        while (indices[current] != i) {
+            size_t next = indices[current];
+
+            wh.id[current] = wh.id[next];
+            wh.name[current] = std::move(wh.name[next]);
+            wh.type[current] = std::move(wh.type[next]);
+            wh.price[current] = wh.price[next];
+
+
+            indices[current] = current;
+
+            current = next;
+        }
+
+        wh.id[current] = tempId;
+        wh.name[current] = std::move(tempName);
+        wh.type[current] = std::move(tempType);
+        wh.price[current] = tempPrice;
+
+        indices[current] = current;
+    }
 }
--- bulk_sort.h
+++ bulk_sort.h
@@ -7,3 +7,5 @@
 #include "ikea_data_struct.h"
 
 void bulkSort(ikeaData& wh, std::string sortBy = "id", size_t stopNum = 0);
+
+void applyPermutation(ikeaData& wh, std::vector<size_t>& indices);
