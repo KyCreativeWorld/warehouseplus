ead8c8f2d7649e51e09a09bf7f6eeed6f84959c0
schrky <Kyle.Schroetlin@wallawalla.edu>
2026-06-04 20:59:30 -0700

    Inb_outb and cleaned up bulk_sort

--- backend/bulk_sort.h
+++ backend/bulk_sort.h
@@ -1,11 +1,9 @@
-// #pragma once
+#pragma once
 
-// #include <vector>
-// #include <string>
-// #include <algorithm>
-// #include <numeric>
-// #include "ikea_data_struct.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include "ikea_data_struct.h"
 
-// void bulkSort(std::vector<ikeaData>& wh, std::string sortBy = "id", size_t stopNum = 0);
-
-// void applyPermutation(std::vector<ikeaData>& wh, std::vector<size_t>& indices);
+void bulkSort(std::vector<ikeaData>& wh);
